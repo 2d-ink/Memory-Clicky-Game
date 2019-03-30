1caaefa862e901ea2850418544cbb58aaf692570
Ryan Nguyen <ryan@Ryans-MacBook-Pro-2.local>
2019-03-30 12:23:44 -0600

    Updates

---
+++ precache-manifest.e7fdfad7d8b353573c9872b754355b51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23d514d4be8ae5243ee5",
+    "url": "/Memory-Clicky-Game/static/js/runtime~main.67ee7de4.js"
+  },
+  {
+    "revision": "101f846fbfa7496ec606",
+    "url": "/Memory-Clicky-Game/static/js/main.0b85e65f.chunk.js"
+  },
+  {
+    "revision": "938ac1ca470ebfb5dc28",
+    "url": "/Memory-Clicky-Game/static/js/2.c8b9e834.chunk.js"
+  },
+  {
+    "revision": "101f846fbfa7496ec606",
+    "url": "/Memory-Clicky-Game/static/css/main.037f7ef8.chunk.css"
+  },
+  {
+    "revision": "3dc84faf6de727b4c8f18105a72f8b15",
+    "url": "/Memory-Clicky-Game/index.html"
+  }
+];
